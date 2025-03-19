b3fdcb80641097f74904bb064a0a019a08f4e400
Gustavo Takahashi <gustavoet@al.insper.edu.br>
2025-03-19 14:02:48 -0300

    feito

--- main/main.c
+++ main/main.c
@@ -1,17 +1,121 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/rtc.h"
+#include "pico/util/datetime.h"
+#include <string.h>
+#define MAX_LEN 10
+
+const int TRIGGER_PIN = 2;
+const int ECHO_PIN = 3;
+volatile uint64_t dt;
+volatile int dt_flag = 0;
+volatile bool timer_fired = false;
+
+
+int64_t alarm_callback(alarm_id_t id, void *user_data) {
+    timer_fired = true;
+    return 0;
+}
+
+void echo_callback(uint gpio, uint32_t events) {
+    if (events & GPIO_IRQ_EDGE_RISE) {
+        dt = to_us_since_boot(get_absolute_time());
+    } else if (events & GPIO_IRQ_EDGE_FALL) {
+        dt = to_us_since_boot(get_absolute_time()) - dt;
+        dt_flag = 1;
+    }
+}
 
 int main() {
     stdio_init_all();
+
+    gpio_init(TRIGGER_PIN);
+    gpio_set_dir(TRIGGER_PIN, GPIO_OUT);
+
+    gpio_init(ECHO_PIN);
+    gpio_set_dir(ECHO_PIN, GPIO_IN);
+    gpio_pull_down(ECHO_PIN);
+
+    double distancia;
+    int flag_running = 0;
+
+    gpio_set_irq_enabled_with_callback(ECHO_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &echo_callback);
+
+    datetime_t t = {
+        .year  = 2024,
+        .month = 1,
+        .day   = 13,
+        .dotw  = 3,
+        .hour  = 11,
+        .min   = 20,
+        .sec   = 0
+    };
+
+    rtc_init();
+    rtc_set_datetime(&t);
+
+    char buffer[MAX_LEN];
+    int caracter;
+
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+
+        int i = 0;
+
+        while (i < MAX_LEN-1) {
+            caracter = getchar_timeout_us(100000);
+            if (caracter == PICO_ERROR_TIMEOUT || caracter == '\n') {
+                buffer[i] = '\0';
+                break;
+            }
+            buffer[i++] = caracter;
+        }
+
+        if (caracter == '\n') {
+            if (buffer[0] == 's' && buffer[1] == '\0') {
+                printf("Inicializando leitura e log\n");
+                flag_running = 1;
+            }
+            else if (buffer[0] == 'p' && buffer[1] == '\0') {
+                printf("Desligando leitura e log\n");
+                flag_running = 0;
+            }
+            while (getchar_timeout_us(0) != PICO_ERROR_TIMEOUT);
+        }
+
+        if (!flag_running) {
+            sleep_ms(100);
+            continue;
+        }
+
+        rtc_get_datetime(&t);
+        char datetime_buf[256];
+        char *datetime_str = &datetime_buf[0];
+        datetime_to_str(datetime_str, sizeof(datetime_buf), &t);
+
+        gpio_put(TRIGGER_PIN, 1);
+        sleep_us(10);
+        gpio_put(TRIGGER_PIN, 0);
+
+
+        if (!dt_flag) {
+            if (!add_alarm_in_us(50000, &alarm_callback, NULL, true)) {
+                printf("Erro ao criar alarme\n");
+            }
+        }
+
+        if (dt_flag) {
+            distancia = dt * 0.017;
+            printf("%s - %.3f cm \n", datetime_str, distancia);
+            dt_flag = 0;
+            timer_fired = false;  
+        }
+
+        if (timer_fired) {
+            printf("%s - Falha \n", datetime_str);
+            timer_fired = false;
+        }
+
+        sleep_ms(50);
     }
 }
